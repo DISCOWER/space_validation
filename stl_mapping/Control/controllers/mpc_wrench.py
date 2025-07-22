small fixes to plotting, changed rotation order

stl_mapping/Control/controllers/mpc_wrench.py
@@ -44,7 +44,7 @@ from ..models_2d.atmos_wrench import atmos_model_wrench
 class MpcWrench():
     def __init__(self):
         # Define the controller parameters
-        self.dt = 0.1               # MPC time step [s]
+        self.dt = 0.2               # MPC time step [s]
         self.Nx = 30                # Prediction horizon, states             
         self.Nu = 30                # Prediction horizon, inputs
         self.Q = np.diag([          # State weighting matrix
@@ -52,7 +52,7 @@ class MpcWrench():
             2e0, 2e0, 1e0, 
             1e2, 
             5e0, 5e0, 5e0])             
-        self.R = 0.1*np.diag([          # State weighting matrix
+        self.R = np.diag([          # State weighting matrix
             1e0, 1e0, 1e0,
             1e0, 1e0, 1e0]) 
         self.P = 20 * self.Q        # Terminal state weighting matrix

stl_mapping/Planning/main.py
@@ -50,25 +50,25 @@ sp_robot = LinearFreeFlyer6DoF()
 # ])
 # spec = Spec(phi, t0, tf)
 
-euler_order = 'xyz'
+euler_order = 'zyx'
 
 X0 = HyperRectangle(np.array([0.5, 0, 0]), np.array([0.6, 0.1, 0.1]))
 Xf = HyperRectangle(np.array([2.5, 1.0, 0]), np.array([2.6, 1.1, 0.1]))
 XA = HyperRectangle(np.array([2.5, -1.0, 0,  -np.pi/2-np.pi/8]), np.array([2.6, -0.9, 0.1,  -np.pi/2+np.pi/8]))
-XA = HyperRectangle(np.array([2.5, -1.0, 0]), np.array([2.6, -0.9, 0.1]))
+# XA = HyperRectangle(np.array([2.5, -1.0, 0]), np.array([2.6, -0.9, 0.1]))
 Obs = []
 World = HyperRectangle(np.array([0, -1.5, -10, -10]), np.array([4, 1.5, 10, 10]))
 phi = Pred("AND", preds=[
     Pred("G", [t0,t0], preds=[Pred("MU", preds=[Polytope(X0)], dims=[0,1,2])]),
     Pred("G", [tf,tf], preds=[Pred("MU", preds=[Polytope(Xf)], dims=[0,1,2])]),
-    Pred("F", [t0,tf], preds=[Pred("MU", preds=[Polytope(XA)], dims=[0,1,2])]), # 3: pitch, 4: roll, 5: yaw
+    Pred("F", [t0,tf], preds=[Pred("MU", preds=[Polytope(XA)], dims=[0,1,2, 3])]), # 3: pitch, 4: roll, 5: yaw
     Pred("G", [t0,tf], preds=[Pred("MU", preds=[Polytope(World)], dims=[0,1,6,7])]),
 ])
 spec = Spec(phi, t0, tf)
 
 
 # create planner
-if False:
+if True:
     opt = gp.Model("prob1")
     x_vars = opt.addMVar((N, sp_robot.n_x), lb=-np.inf, ub=np.inf, name="X")
     u_vars = opt.addMVar((N, sp_robot.n_u), lb=-np.inf, ub=np.inf, name="U")
@@ -100,7 +100,7 @@ if False:
     opt.addConstr(act_int_var == gp.quicksum([act_abs_var[i,j] for i in range(N) for j in range(sp_robot.n_u)]), name="act_int_sum")
     opt.addConstr(cost_var == 1*alpha_vars - 10000*spec.phi.rho + 0.0001*act_int_var) # quad_cost
     opt.setObjective(cost_var, gp.GRB.MINIMIZE)
-    opt.setParam('OutputFlag', 1)  # Suppress Gurobi output
+    opt.setParam('OutputFlag', 0)  # Suppress Gurobi output
     opt.optimize()
     if opt.status == gp.GRB.OPTIMAL:
         print(f"Optimal solution found")
@@ -138,9 +138,9 @@ plot_planning_results(sp_robot, t_sp, x_ff, u_ff, X0, Xf, [XA], Obs, alpha=alpha
 x_ff_quat = np.zeros((x_ff.shape[0], x_ff.shape[1] + 1))
 for i in range(x_ff.shape[0]):
     x_ff_quat[i,:] = euler_x_to_quat_x_np(x_ff[i, :],order=euler_order)
-# print(f"euler: {x_ff[:, 3:6]}")
-# print(f"quat:  {x_ff_quat[:, 3:7]}")
-# print(f"euler: {np.array([quat_to_euler_np(x[3:7], order=euler_order) for x in x_ff_quat])}")
+print(f"euler: {x_ff[:, 3:6]}")
+print(f"quat:  {x_ff_quat[:, 3:7]}")
+print(f"euler: {np.array([quat_to_euler_np(x[3:7], order=euler_order) for x in x_ff_quat])}")
 
 np.savez('stl_mapping/Planning/solutions/sp_solution_quat.npz', x_ff=x_ff_quat, u_ff=u_ff, dt=dt, alpha=alpha, times=t_sp)
 
@@ -190,6 +190,20 @@ u_max = np.max(np.abs(u_uw), axis=0)
 print(f"\nAlpha if u_fbl(x_sp,u_sp) applied to BlueROV directly: {np.max(u_max/uw_robot.U.upper_bounds)}")
 print(f"U should become {np.max(u_max)/alpha} for same alpha (was {uw_robot.U.upper_bounds[0]}) which is {(np.max(u_max)/alpha)/uw_robot.U.upper_bounds[0]*100}%")
 
+
+# # test if different backends are equal
+# uw_robot_cs = BlueROV(backend='cs')
+# uw_robot_np = BlueROV(backend='np')
+# for i in range(100):
+#     x = np.random.rand(13)
+#     u = np.random.rand(6)
+#     x_cs = cs.DM(x)
+#     u_cs = cs.DM(u)
+#     x_uw_cs = uw_robot_cs.step(x_cs, u_cs, dt)
+#     x_uw_np = uw_robot_np.step(x, u, dt)
+#     print(f"Step {i}: {x_uw_cs} == {x_uw_np}")
+
+
 # So now we have to find \phi (for now deltaT) such that
 # alpha for uw is equal to alpha for the free flyer
 # we have bilinear constraints so that is quite tricky but
@@ -211,16 +225,20 @@ if True:
     ocp.subject_to(dt_vars >= 0)
     ocp.subject_to(delta >= 0)
 
-    # for i in range(N):
-    #     ocp.subject_to(u_fbl_cs(x_vars[:,i],u_vars[:,i])[0:2] >= alpha*(uw_robot.U.lower_bounds[0:2]))
-    #     ocp.subject_to(u_fbl_cs(x_vars[:,i],u_vars[:,i])[0:2] <= alpha*(uw_robot.U.upper_bounds[0:2]))
+    for i in range(N):
+        # ocp.subject_to(u_fbl_cs(x_vars[:,i],u_vars[:,i])[0:2] >= alpha*(uw_robot.U.lower_bounds[0:2]))
+        # ocp.subject_to(u_fbl_cs(x_vars[:,i],u_vars[:,i])[0:2] <= alpha*(uw_robot.U.upper_bounds[0:2]))
+        # ocp.subject_to(u_vars[:,i] >= alpha * uw_robot.U.lower_bounds)
+        # ocp.subject_to(u_vars[:,i] <= alpha * uw_robot.U.upper_bounds)
+        ocp.subject_to(u_vars[:,i] >= alpha * u_fbl_cs(x_vars[:,i],uw_robot.U.lower_bounds))
+        ocp.subject_to(u_vars[:,i] <= alpha * u_fbl_cs(x_vars[:,i],uw_robot.U.upper_bounds))
 
     # constraints
     for i in range(N-1):
         ocp.subject_to(x_vars[:,i+1] == uw_robot.step(x_vars[:,i], u_vars[:,i], dt_vars))
     
     # enforce position equality with the free flyer
-    n_equal = 6 # first n_equal states 
+    n_equal = 7 # first n_equal states 
     for i in range(N):
         ocp.subject_to(x_vars[0:n_equal,i] <= x_uw_fbl_sp[i,0:n_equal] + delta* np.ones(n_equal))
         ocp.subject_to(x_vars[0:n_equal,i] >= x_uw_fbl_sp[i,0:n_equal] - delta* np.ones(n_equal))
@@ -240,6 +258,7 @@ if True:
         dt_uw = sol.value(dt_vars)
         t_uw = np.linspace(0, (N-1)*dt_uw, N)
         print(f"delta: {sol.value(delta)}")
+        print(f"dt_uw: {dt_uw}")
         # print(f"Solution found")
     except Exception as e:
         print(f"Solution not found: {e}")

stl_mapping/Utilities/Robots.py
@@ -123,7 +123,8 @@ class LinearFreeFlyer6DoF(Robot):
             np.array([max_torque]*3) * scale_torque
         ])
         self.U = HyperRectangle(-u_max, u_max)
-        self.D = HyperRectangle(np.array([-1, -1, -1]),np.array([1, 1, 1]))
+        max_floor_force = (36*self.mass)/1000
+        self.D = HyperRectangle(np.array(3*[-max_floor_force]),np.array(3*[max_floor_force]))
 
         # self.U_effective = minkowski_difference(self.U, self.K@self.D)
         # TODO: deal with the fact that K and D are not of same dimension
@@ -160,6 +161,8 @@ class LinearFreeFlyer6DoF(Robot):
 
 class FreeFlyer(Robot):
     def __init__(self, backend='np'):
+        # state: x = [p, q, v, w]
+        # where q is a scalar first unit quaternion
         nx = 13
         nu = 6
         super().__init__(n_x=nx, n_u=nu)