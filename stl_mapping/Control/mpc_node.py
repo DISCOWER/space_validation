fixed relative weighing in planner, added ekf covariance to topic

stl_mapping/Control/mpc_node.py
@@ -9,7 +9,7 @@ from Utilities.get_reference_trajectory import get_reference_trajectory, Referen
         
 from nav_msgs.msg import Path
 from visualization_msgs.msg import Marker
-from geometry_msgs.msg import PoseStamped, WrenchStamped
+from geometry_msgs.msg import PoseStamped, WrenchStamped, TwistWithCovarianceStamped
 from std_msgs.msg import Bool
 from px4_msgs.msg import VehicleStatus, VehicleAttitude, VehicleAngularVelocity, VehicleLocalPosition
 from px4_msgs.msg import VehicleThrustSetpoint, VehicleTorqueSetpoint, OffboardControlMode, ActuatorMotors
@@ -58,6 +58,11 @@ class MPCNode(Node):
             VehicleAngularVelocity,
             'fmu/out/vehicle_angular_velocity',
             self.vehicle_angular_velocity_callback,
+            NORMAL_QOS)
+        self.local_position_sub_v1 = self.create_subscription(
+            VehicleLocalPosition,
+            'fmu/out/vehicle_local_position',
+            self.vehicle_local_position_callback,
             NORMAL_QOS)
         self.local_position_sub_v1 = self.create_subscription(
             VehicleLocalPosition,
@@ -119,19 +124,20 @@ class MPCNode(Node):
             "force_torque_cmd",
             10)
         self.disturbance_est_pub = self.create_publisher(
-            WrenchStamped,
+            TwistWithCovarianceStamped,
             'disturbance_estimate',
             10)
         
         self.get_logger().info("MPC publishers initialized successfully")
 
         # Disturbance variables
-        self.offset_free = False
+        self.offset_free = True
         self.F_cmd = np.zeros((3, 1))  # Force commanded
         self.T_cmd = np.zeros((3, 1))  # Torque commanded
         self.ekf_estimator = EKFWrenchEstimator(dt=0.05)
         self.fd_est = np.zeros(3)  # Estimated force disturbance
         self.td_est = np.zeros(3)  # Estimated torque disturbance
+        self.dist_cov = np.zeros((6, 6))  # Disturbance covariance matrix
 
         # Create the MPC solver and create timer callback to solve
         timer_period = 0.2 # seconds
@@ -236,17 +242,28 @@ class MPCNode(Node):
         else:
             self.get_logger().info("Received stop signal, stopping MPC computation.")
 
-    def publish_estimated_disturbance(self, fd_est, td_est):
-        wrench_msg = WrenchStamped()
+    def publish_estimated_disturbance(self, fd_est, td_est, dist_cov):
+        # wrench_msg = WrenchStamped()
+        # wrench_msg.header.stamp = self.get_clock().now().to_msg()
+        # wrench_msg.header.frame_id = 'map'
+
+        # wrench_msg.wrench.force.x = float(fd_est[0])
+        # wrench_msg.wrench.force.y = float(fd_est[1])
+        # wrench_msg.wrench.force.z = float(fd_est[2])
+        # wrench_msg.wrench.torque.x = float(td_est[0])
+        # wrench_msg.wrench.torque.y = float(td_est[1])
+        # wrench_msg.wrench.torque.z = float(td_est[2])
+        wrench_msg = TwistWithCovarianceStamped()
         wrench_msg.header.stamp = self.get_clock().now().to_msg()
         wrench_msg.header.frame_id = 'map'
 
-        wrench_msg.wrench.force.x = float(fd_est[0])
-        wrench_msg.wrench.force.y = float(fd_est[1])
-        wrench_msg.wrench.force.z = float(fd_est[2])
-        wrench_msg.wrench.torque.x = float(td_est[0])
-        wrench_msg.wrench.torque.y = float(td_est[1])
-        wrench_msg.wrench.torque.z = float(td_est[2])
+        wrench_msg.twist.twist.linear.x = float(fd_est[0])
+        wrench_msg.twist.twist.linear.y = float(fd_est[1])
+        wrench_msg.twist.twist.linear.z = float(fd_est[2])
+        wrench_msg.twist.twist.angular.x = float(td_est[0])
+        wrench_msg.twist.twist.angular.y = float(td_est[1])
+        wrench_msg.twist.twist.angular.z = float(td_est[2])
+        wrench_msg.twist.covariance = dist_cov.flatten().tolist() + [0.0] * (36 - len(dist_cov.flatten()))
 
         self.disturbance_est_pub.publish(wrench_msg)
 
@@ -262,8 +279,8 @@ class MPCNode(Node):
         u[3:6] /= T_scaling
 
         # ENU -> NED transformation
-        force_output_msg.xyz = [u[0]*0.5, -u[1]*0.5, -u[2]*0.5]
-        torque_output_msg.xyz = [u[3]*0.5, -u[4]*0.5, -u[5]*0.5]
+        force_output_msg.xyz = [u[0], -u[1], -u[2]]
+        torque_output_msg.xyz = [u[3], -u[4], -u[5]]
         # EKF in FLU frame so don't transform
         # self.F_app = u[:3]
         # self.T_app = u[3:6]
@@ -354,8 +371,8 @@ class MPCNode(Node):
                            self.vehicle_angular_velocity[0],
                            self.vehicle_angular_velocity[1],
                            self.vehicle_angular_velocity[2]]).reshape(13, 1)
-        self.fd_est, self.td_est = self.ekf_estimator.step(x0.flatten(), self.F_cmd.flatten(), self.T_cmd.flatten())
-        self.publish_estimated_disturbance(self.fd_est, self.td_est)
+        self.fd_est, self.td_est, self.dist_cov = self.ekf_estimator.step(x0.flatten(), self.F_cmd.flatten(), self.T_cmd.flatten())
+        self.publish_estimated_disturbance(self.fd_est, self.td_est, self.dist_cov)
 
     def cmdloop_callback(self):
         # self.get_logger().info("Command loop callback")

stl_mapping/Planning/main.py
@@ -35,9 +35,10 @@ sp_robot = LinearFreeFlyer6DoF()
 euler_order = 'xyz'
 
 # STL Specification
-X0 = HyperRectangle(np.array([0.5, 0, 2.0]), np.array([0.6, 0.1, 2.1]))
-Xf = HyperRectangle(np.array([2.5, 1.0, 2.0]), np.array([2.6, 1.1, 2.1]))
-XA = HyperRectangle(np.array([2.5, -1.0, 2.0,  -np.pi/2-np.pi/8]), np.array([2.6, -0.9, 2.1,  -np.pi/2+np.pi/8]))
+depth = 0.0
+X0 = HyperRectangle(np.array([0.5, 0, depth]), np.array([0.6, 0.1, depth+0.1]))
+Xf = HyperRectangle(np.array([2.5, 1.0, depth]), np.array([2.6, 1.1, depth+0.1]))
+XA = HyperRectangle(np.array([2.5, -1.0, depth,  -np.pi/2-np.pi/8]), np.array([2.6, -0.9, depth+0.1,  -np.pi/2+np.pi/8]))
 # later converted to polytopes for predicates of the form Ax \leq b: Polytope = (A,b)
 # XA = HyperRectangle(np.array([2.5, -1.0, 2.0]), np.array([2.6, -0.9, 2.1]))
 Obs = []
@@ -87,9 +88,12 @@ if True:
 
     # L1 actuation cost
     act_absu_vars = opt.addMVar((N-1,sp_robot.n_u))
+    act_absu_scaled_vars = opt.addMVar((N-1, sp_robot.n_u), lb=0, ub=np.inf)
     act_absu_var = opt.addVar(lb=0, ub=np.inf)
+    Q = np.array([1., 1., 1., 10., 10., 10.])
     opt.addConstrs((act_absu_vars[i, j] == gp.abs_(u_vars[i, j]) for i in range(N-1) for j in range(sp_robot.n_u)), name="act_abs")
-    opt.addConstr(act_absu_var == gp.quicksum([act_absu_vars[i, j] for i in range(N-1) for j in range(sp_robot.n_u)]), name="act_abs_sum")
+    opt.addConstrs((act_absu_scaled_vars[i, j] == Q[j]*act_absu_vars[i, j] for i in range(N-1) for j in range(sp_robot.n_u)), name="act_abs_scaled")
+    opt.addConstr(act_absu_var == gp.quicksum([act_absu_scaled_vars[i, j] for i in range(N-1) for j in range(sp_robot.n_u)]), name="act_abs_sum")
 
     # Final cost
     opt.addConstr(cost_var == 1*alpha_vars - 1000*spec.phi.rho + 0.01*act_absu_var) # quad_cost
@@ -122,6 +126,11 @@ else:
     alpha = data['alpha']
     dt = data['dt']
     t_sp = data['times']
+    
+# print(f"Euler angles: {np.rad2deg(x_ff[:, 5])}")
+
+plot_planning_results(sp_robot, t_sp, x_ff, u_ff, X0, Xf, [XA], Obs, alpha=alpha,
+                      path="stl_mapping/Planning/figures/atmos_trajectory_euler.png")
 
 #! Convert [p:ENU, e:FLU->ENU, v:ENU, w:FLU] to [p:ENU, q:FLU->ENU, v:ENU, w:FLU]
 x_ff_converted = np.zeros((x_ff.shape[0], x_ff.shape[1] + 1))

stl_mapping/Utilities/plotting.py
@@ -6,18 +6,34 @@ def plot_planning_results(robot, t, x, u,
                           X0=None, Xf=None, ROIs=None, Obs=None, alpha=1.0,
                           in_axs = None, plot=True,
                           path="stl_mapping/Planning/figures/sp_trajectory.png"):
+    # if x.shape[1] == 12:
+    #     p, e, v, w = x[:, 0:3], x[:, 3:6], x[:, 6:9], x[:, 9:12]
+    #     q = np.zeros((x.shape[0], 4))
+    #     for i in range(x.shape[0]):
+    #         q[i, :] = euler_to_quat_np(e[i, :], order='zyx')
+    #     x = np.hstack((p, q, v, w))
+    #     print(f"Converted state representation:\n{x}")
+    # elif x.shape[1] == 13:
+    #     p, q, v, w = x[:, 0:3], x[:, 3:7], x[:, 7:10], x[:, 10:13]
+    # else:
+    #     raise ValueError("State vector x must have 12 or 13 columns.")
     if x.shape[1] == 12:
-        p, e, v, w = x[:, 0:3], x[:, 3:6], x[:, 6:9], x[:, 9:12]
-        q = np.zeros((x.shape[0], 4))
-        for i in range(x.shape[0]):
-            q[i, :] = euler_to_quat_np(e[i, :], order='zyx')
-        x = np.hstack((p, q, v, w))
-        print(f"Converted state representation:\n{x}")
+        p_idxs = [0, 1, 2]
+        q_idxs = [3, 4, 5]
+        v_idxs = [6, 7, 8]
+        w_idxs = [9, 10, 11]
     elif x.shape[1] == 13:
-        p, q, v, w = x[:, 0:3], x[:, 3:7], x[:, 7:10], x[:, 10:13]
+        p_idxs = [0, 1, 2]
+        q_idxs = [3, 4, 5, 6]
+        v_idxs = [7, 8, 9]
+        w_idxs = [10, 11, 12]
     else:
         raise ValueError("State vector x must have 12 or 13 columns.")
     
+    p = x[:, p_idxs]
+    q = x[:, q_idxs]
+    v = x[:, v_idxs]
+    w = x[:, w_idxs]
 
     # plot the trajectory
     if in_axs is None:
@@ -58,9 +74,13 @@ def plot_planning_results(robot, t, x, u,
     ax_q.plot(t, q[:, 0], 'r', label='q0')
     ax_q.plot(t, q[:, 1], 'g', label='q1')
     ax_q.plot(t, q[:, 2], 'b', label='q2')
-    ax_q.plot(t, q[:, 3], 'k', label='q3')
+    if q.shape[1] == 4:  # If quaternion representation
+        ax_q.plot(t, q[:, 3], 'k', label='q3')
     ax_q.set_xlabel('Time (s)')
-    ax_q.set_ylabel('Euler angles (rad)')
+    if q.shape[1] == 4:
+        ax_q.set_ylabel('Quaternion (q0, q1, q2, q3)')
+    else:
+        ax_q.set_ylabel('Euler angles (pitch roll yaw)')
     ax_q.legend()
     ax_q.grid()
 